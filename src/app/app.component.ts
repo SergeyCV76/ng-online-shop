cd9d84452875bd5adfe9312e28293bba3888ce06
SergeyCV76 <SergeyCV76@gmail.com>
2024-12-19 10:50:35 +0200

    add navigation and routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatInputModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'ng-online-shop';
+export class AppComponent implements OnInit{
+  public navItems =
+  [
+    {
+      title: 'Main',
+      route: '',
+      icon: 'dashboard',
+    },
+    {
+      title: 'Products',
+      route: 'products',
+      icon: 'apps',
+    },
+    {
+      title: 'Basket',
+      route: 'basket',
+      icon: 'shopping_cart',
+    },
+    {
+      title: 'Admin Panel',
+      route: 'adminPanel',
+      icon: 'admin_panel_settings',
+    },
+  ]
+    constructor() {}
+
+    ngOnInit() {}
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
+import { BasketComponent } from './pages/basket/basket.component';
+import { AdminPanelComponent } from './pages/admin-panel/admin-panel.component';
+import { UserLoginComponent } from './pages/user-login/user-login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'product/:id', component: ProductDetailsComponent},
+  {path: 'basket', component: BasketComponent},
+  {path: 'userLogin', component: UserLoginComponent},
+  {path: 'adminPanel', component: AdminPanelComponent},
+  {path: '**', redirectTo: '', component: MainComponent}
+];
